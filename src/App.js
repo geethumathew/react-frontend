c94352e700106429def30d986fd21822ad715253
geethuelizebeth <geethuelizebeth@gmail.com>
2024-04-22 12:51:37 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+
+import { Component } from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      My todo application 
+      <FirstComponent></FirstComponent>
+      <SecondComponet></SecondComponet>
     </div>
   );
 }
 
+function FirstComponent(){
+  return (
+    <div className="FirstComponent">
+   FirstComponent
+  </div>
+  )
+}
+
+class SecondComponet extends Component{
+  render(){
+  return (
+    <div className="SecondComponet">SecondComponet</div>
+  )
+}
+}
 export default App;
